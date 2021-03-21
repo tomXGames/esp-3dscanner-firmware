591ccd46395cfd89f119fe1cf78765805bcfc714
tomXGames <tommaso.peduzzi@gmx.ch>
2021-03-21 21:30:07 +0100

    Changed bluetoooth name

--- src/firmware/firmware.ino
+++ src/firmware/firmware.ino
@@ -18,7 +18,7 @@ BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
+  SerialBT.begin("ESP32 3D-Scanner"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
