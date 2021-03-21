0d77222e128c7c8ced9e2b27ecf713045480c625
tomXGames <tommaso.peduzzi@gmx.ch>
2021-03-21 21:00:51 +0100

    kinda works

--- src/firmware/firmware.ino
+++ src/firmware/firmware.ino
@@ -1,20 +1,21 @@
 #include "BluetoothSerial.h"
+#include <Stepper.h>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-#include <Stepper.h>
 const int stepsPerRevolution = 2048;
 int motorPin1 = 13;
 int motorPin2 = 12;
 int motorPin3 = 14;
 int motorPin4 = 27;
-Stepper stepper(stepsPerRevolution, motorPin1, motorPin2, motorPin3, motorPin4);
-int noSteps;
-BluetoothSerial SerialBT;
-bool turning = true;
 int motorSpeed = 2;
+int noSteps;
+bool turning = true;
+Stepper stepper(stepsPerRevolution, motorPin1, motorPin2, motorPin3, motorPin4);
+BluetoothSerial SerialBT;
+
 void setup() {
   Serial.begin(115200);
   SerialBT.begin("ESP32test"); //Bluetooth device name
@@ -22,24 +23,20 @@ void setup() {
 }
 
 void loop() {
-  if (SerialBT.available() > 0) {
-    String str = SerialBT.readString();
-    Serial.println(str);
-    if (str == "go\n") {
-      turning = true;
-    }
-    else if (str == "stop") {
-      turning = false;
-    }
-    else if(str != ""){
-      motorSpeed = str.toInt();
-    }
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
   }
+  if (SerialBT.available()) {
+    int out = SerialBT.read();
+    Serial.println(out);
+    if(out != 0) turning != turning;
+    else motorSpeed = out;
+  }
+  delay(20);
   if (turning) {
     turn();
   }
-//  stepper.step(stepsPerRevolution);
-//  delay(500);
+
 }
 void turn() {
   // 1
